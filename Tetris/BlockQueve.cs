b79eed3047c58e0bd3632fb6efbbe5ce58276dcc
Stefano Orsini <orsinistefanocalc@gmail.com>
2023-12-16 14:14:35 +0100

    caricamento del gioco

---
+++ Tetris/BlockQueve.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class BlockQueve
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueve() 
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
